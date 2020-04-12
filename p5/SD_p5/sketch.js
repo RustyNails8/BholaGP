9483141c29866dfcf10afc651071d67d6a0dea72
Sumit Das <sumit8das@gmail.com>
2020-04-12 19:38:13 +0530

    Add p5 Java Script

---
+++ p5/SD_p5/sketch.js
@@ -0,0 +1,26 @@
+var x, r, y 
+
+function setup() {
+  createCanvas(1200, 600);
+    
+    noStroke();
+    background('rgba( 221, 23, 191, 0.508 )');
+    y=0;
+}
+
+function draw() {
+    x= random(width);
+    
+    if (random() > 0.7) {
+        r = random(6,10);
+    } else {
+        r = random(1,5);
+    }
+    
+    ellipse(x, y, r, r);
+    y = y +4;
+    
+    if (y > 1024){
+        y=0;
+    }
+    }
