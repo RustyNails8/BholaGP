b8a7953b98e0fcb3073e95114c3bf2515860bcb9
Sumit Das <sumit8das@gmail.com>
2020-04-12 20:23:54 +0530

    Live Bhola p5

--- p5/SD_p5/sketch.js
+++ p5/SD_p5/sketch.js
@@ -1,26 +1,12 @@
-var x, r, y 
-
 function setup() {
-  createCanvas(1200, 600);
-    
-    noStroke();
-    background('rgba( 221, 23, 191, 0.508 )');
-    y=0;
+  createCanvas(1024, 1024);
 }
 
 function draw() {
-    x= random(width);
-    
-    if (random() > 0.7) {
-        r = random(6,10);
-    } else {
-        r = random(1,5);
-    }
-    
-    ellipse(x, y, r, r);
-    y = y +4;
-    
-    if (y > 1024){
-        y=0;
-    }
-    }
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 10, 10);
+}
