28e02fc3972cfbf9d01a57aecfababf078ab2812
Sumit Das <sumit8das@gmail.com>
2020-05-02 01:27:50 +0530

    Seperate py files

---
+++ py/TwoLines.py
@@ -0,0 +1,12 @@
+from graphics import *
+
+def main():
+    win = GraphWin("My Two Lines", 100, 100)
+    l1 = Line(Point(10,30), Point(20,90))
+    l2 = Line(Point(10,30), Point(50,90))
+    l1.draw(win)
+    l2.draw(win)
+    win.getMouse() # pause for click in window
+    win.close()
+
+main()
